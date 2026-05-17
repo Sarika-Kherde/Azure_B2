30170f26e76e221f280517936815d05cbc732f70
Sarika-Kherde <kherdesarika@gmail.com>
2026-05-17 05:18:24 +0000

    Changed notebook format

---
+++ Read_Data_Using_Access_Token.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+# /// script
+# [tool.databricks.environment]
+# environment_version = "5"
+# ///
+# MAGIC %md
+# MAGIC ## This Notebook is used to read data from ADLS using access key
+
+# COMMAND ----------
+
+
