9765a32cac801a40875eb7691bd3c6e088a2f7f0
Sarika-Kherde <kherdesarika@gmail.com>
2026-05-17 05:25:01 +0000

    Modified Notebook

--- Read_Data_Using_Access_Token.py
+++ Read_Data_Using_Access_Token.py
@@ -8,4 +8,14 @@
 
 # COMMAND ----------
 
+lst = [1,2,3,4,5,6]
+even = []
+for i in lst:
+    if i%2 == 0:
+        even.append(i)
+print(even)
 
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from Customer_df where city ='Pune';
